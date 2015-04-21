974487efe2d99d865e5814179a14e6e907c08912
Krzysztof Magosa <krzysztof@magosa.pl>
2015-04-21 23:57:39 +0200

    +init

--- src/graver/edge.py
+++ src/graver/edge.py
@@ -8,13 +8,13 @@ class Edge:
         self.source.edges.add(self)
         self.target.edges.add(self)
 
+    def get_endpoints(self):
+        return [self.target] if self.oneway else [self.source, self.target]
+
     def get_successor(self, opposite=None):
-        vertices = set([self.target])
+        vertices = set(self.get_endpoints())
 
-        if not self.oneway:
-            vertices.add(self.source)
-
-        if opposite is not None and opposite in vertices:
+        if opposite in vertices:
             vertices.remove(opposite)
 
         return vertices.pop() if len(vertices) else None
